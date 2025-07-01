fca18053014fc8c455bda675421723db97403484
MxAziz <hereabdulaziz@gmail.com>
2025-07-01 10:07:43 +0600

    added async function and use try-catch block to get a proper response

--- src/modules/books/books.controller.ts
+++ src/modules/books/books.controller.ts
@@ -1,14 +1,21 @@
 import { Request, Response } from "express";
 import BOOK from "./book.model";
 
-const createBook = (req: Request, res: Response) => {
-    const book = BOOK.create(req.body);
-
-    res.status(201).json({
-        success: true,
-        message: "book created successfully",
-        data: book,
-    })
+const createBook = async (req: Request, res: Response) => {
+    try {
+        const book = await BOOK.create(req.body);
+        res.status(201).json({
+            success: true,
+            message: "book created successfully",
+            data: book,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Failed to create book",
+            error
+        });
+    }
 };
 
 
