4098cb4817e31ff01de5380f4af21c609a9b3125
MxAziz <hereabdulaziz@gmail.com>
2025-07-02 20:05:33 +0600

    added borrow route file and its basic structure

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { BooksRoute } from '../modules/books/book.routes';
+import { BorrowRoute } from '../modules/borrow-books/borrow.routes';
 // import bookingRoutes from "./booking.route";
 
 const routes = express.Router();
 
 // routes.use("/bookings", bookingRoutes);
 routes.use('/books', BooksRoute)
+routes.use('/borrow', BorrowRoute);
 
 export default routes;
