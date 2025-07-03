a0aa5a21782ad314b1f6df76446cc8f3de992eca
MxAziz <hereabdulaziz@gmail.com>
2025-07-03 11:29:21 +0600

    successfully borrow a book and get a proper response :)

--- src/modules/borrow-books/borrow.routes.ts
+++ src/modules/borrow-books/borrow.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import { borrowController } from "./borrow.controller";
 
 const router = Router();
 
+router.post("/", borrowController.borrowBook)
 
 
 export const BorrowRoute = router;
